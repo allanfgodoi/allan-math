95b56644e86c53763e24a1b8e3a5c92de03b6b99
allanfgodoi <allan.godoi@ufrgs.br>
2022-11-14 00:34:53 -0300

    Added the mean operation.

--- allanmath.cpp
+++ allanmath.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 float power(float a, float b) {
     float result = a;
-
     for (int i = 1; i < b; i++)
         result = result * a;
 
     return result;
+}
+
+float mean(vector<float> v) {
+    float counter = 0;
+    for (int j = 0; j < v.size(); j++)
+        counter += v[j];
+
+    float mean = (counter / v.size());
+
+    return mean;
 }
